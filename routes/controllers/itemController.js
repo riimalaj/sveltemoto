b22eea4c7580c8df34da40e31b54ad16c57a6878
Jouni <jriimala@gmail.com>
2021-12-24 16:06:23 +0200

    logs and errors, wip

--- routes/controllers/itemController.js
+++ routes/controllers/itemController.js
@@ -1,55 +1,81 @@
-import {Context} from 'https://deno.land/x/oak@v9.0.1/mod.ts';
+import { Context } from 'https://deno.land/x/oak@v9.0.1/mod.ts';
 import * as itemServices from "../../services/itemService.js";
 import { renderFile } from "../../deps.js";
+import { ensureDir } from "https://deno.land/std@0.54.0/fs/ensure_dir.ts";
 
 
+var error = [];
 
 const showMain = async ({ response }) => {
-    response.body = await renderFile('../views/start.eta');    
+    response.body = await renderFile('../views/start.eta');
 };
 
-
-const addIdea = async({request, response}) => {
-    console.log("Idean lisäys");
-    const body = request.body();    
-    const formData = await body.value;
-    const idea = formData.get("toive").trim();
-    const esittaja = formData.get("esittaja").trim();
-    var ideaStatus = Boolean(true);
-    var orderStatus = Boolean(false);
-    var deliveredStatus = Boolean(false);
-    console.log('itemController -> ' + idea + ", " + esittaja + ",ideaStatus-> " + ideaStatus, " ,orderStatus->" + orderStatus + ", deliveredStatus->" + deliveredStatus);
-    await itemServices.addIdea(idea, esittaja, ideaStatus, orderStatus, deliveredStatus);
-    response.redirect('/ideas');
+const addIdea = async ({ request, response }) => {
+    try {
+        console.log("Idean lisäys");
+        const body = request.body();
+        const formData = await body.value;
+        const idea = formData.get("toive").trim();
+        const esittaja = formData.get("esittaja").trim();
+        var ideaStatus = Boolean(true);
+        var orderStatus = Boolean(false);
+        var deliveredStatus = Boolean(false);
+        console.log('itemController -> ' + idea + ", " + esittaja + ",ideaStatus-> " + ideaStatus, " ,orderStatus->" + orderStatus + ", deliveredStatus->" + deliveredStatus);
+        await itemServices.addIdea(idea, esittaja, ideaStatus, orderStatus, deliveredStatus);
+        response.redirect('/ideas');
+    }
+    catch (err) {
+        error.push(err);
+    }
 };
 
-const getIdeas = async({response}) => {
+const getIdeas = async ({ response }) => {
     response.body = await renderFile("../views/ideas.eta", {
         ideas: await itemServices.fetchIdeas(),
     });
 };
 
-const getOrders = async({params, response}) => {        
-    console.log("itemController, getOrders -> params.id = " + params.id);
-    response.body = await renderFile("../views/orders.eta", {
-        ordered: await itemServices.fetchOrders(params.id),
-    });
+const getOrders = async ({ params, response }) => {
+    try {
+        console.log("itemController, getOrders -> params.id = " + params.id);
+        response.body = await renderFile("../views/orders.eta", {
+            ordered: await itemServices.fetchOrders(params.id),
+        });
+    }
+    catch (err) {
+        error.push(err);
+    }
 };
 
-const getDelivered = async({params, response}) => {    
+const getDelivered = async ({ params, response }) => {
+    try {
         console.log("itemController, getDelivered -> params.id = " + params.id);
-        response.body = await renderFile("../views/delivered.eta",{
-        deliveries: await itemServices.fetchDelivered(params.id),
-        });            
+        response.body = await renderFile("../views/delivered.eta", {
+            deliveries: await itemServices.fetchDelivered(params.id),
+        });
+    }
+    catch (err) {
+        error.push(err);
+    }
+
 };
 
-const doDelete = async ({response}) => {
-    console.log("itemController, doDelete");
-    response.body = await renderFile('../views/index.eta', {
-        deleteResp : await itemServices.deleteAll(),
-    });
-    
-    
+const doDelete = async ({ response }) => {
+    try {
+        console.log("itemController, doDelete");
+        response.body = await renderFile('../views/index.eta', {
+            deleteResp: await itemServices.deleteAll(),
+        });
+    }
+    catch (err) {
+        error.push(err);
+    }
 }
 
-export {showMain, getIdeas, getOrders, getDelivered, addIdea, doDelete};
+const today = Date.now();
+
+ensureDir("./errors")
+.then(() => Deno.writeTextFile("./errors/appi_errors_" + today + ".log", error));
+
+
+export { showMain, getIdeas, getOrders, getDelivered, addIdea, doDelete };
--- services/itemService.js
+++ services/itemService.js
@@ -1,82 +1,132 @@
 
-import {client} from "../database/db.js";
+import { client } from "../database/db.js";
+import { ensureDir } from "https://deno.land/std@0.54.0/fs/ensure_dir.ts";
 
+var logs = [];
+var error = [];
 //const databaseUrl = Deno.env.get("DATABASE_URL");
 
-const addIdea = async(idea, esittaja, ideaStatus, orderStatus, deliveredStatus) => {
-    console.log('Syötetään lista tauluun -> ' +  idea + ", " + esittaja + ",ideaStatus-> " + ideaStatus, " ,orderStatus->" + orderStatus + ", deliveredStatus->" + deliveredStatus);
-    await client.connect();
-    await client.queryArray('INSERT INTO lista (toive, esittaja, ideastatus, orderstatus, deliveredstatus) VALUES($1, $2, $3, $4, $5)', idea, esittaja, ideaStatus, orderStatus, deliveredStatus);
-    await client.end();
-    await fetchIdeas();
+const addIdea = async (idea, esittaja, ideaStatus, orderStatus, deliveredStatus) => {
+    try {
+        console.log('Syötetään lista tauluun -> ' + idea + ", " + esittaja + ",ideaStatus-> " + ideaStatus, " ,orderStatus->" + orderStatus + ", deliveredStatus->" + deliveredStatus);
+        await client.connect();
+        await client.queryArray('INSERT INTO lista (toive, esittaja, ideastatus, orderstatus, deliveredstatus) VALUES($1, $2, $3, $4, $5)', idea, esittaja, ideaStatus, orderStatus, deliveredStatus);
+        await client.end();
+        await fetchIdeas();
+        logs.push("Idea added");
+    }
+    catch (err) {
+        error.push(err);
+    }
+
 }
 
 const fetchIdeas = async () => {
-    console.log("ideoiden haku");
-    await client.connect();
-    const res = await client.queryArray('SELECT * from lista WHERE ideaStatus = true');
-    await client.end();
-    console.log("Idea list following -> " + res.rows);
-    return res.rows;
+    try {
+        console.log("ideoiden haku");
+        await client.connect();
+        const res = await client.queryArray('SELECT * from lista WHERE ideaStatus = true');
+        await client.end();
+        console.log("Idea list following -> " + res.rows);
+        return res.rows;
+    }
+    catch (err) {
+        error.push(err);
+    }
+
 }
 
-const changeOrderService = async(id) => {
-    console.log("Starting to update ordered status values");
-    await client.connect();
-    await client.queryArray("UPDATE lista SET ideastatus = false, orderstatus = true, deliveredstatus = false WHERE id = $1", id);        
-    await client.end();
-    console.log("Order status updated");    
+const changeOrderService = async (id) => {
+    try {
+        console.log("Starting to update ordered status values");
+        await client.connect();
+        await client.queryArray("UPDATE lista SET ideastatus = false, orderstatus = true, deliveredstatus = false WHERE id = $1", id);
+        await client.end();
+        console.log("Order status updated");
+    }
+    catch (err) {
+        error.push(err);
+    }
+
 }
 
-const changeDeliveredService = async(id) => {
-    console.log("Updating table delivered status value");
-    await client.connect();
-    await client.queryArray("UPDATE lista SET ideastatus = false, orderstatus = false, deliveredstatus = true WHERE id = $1", id);        
-    await client.end();
-    console.log("Delivery status updated");    
+const changeDeliveredService = async (id) => {
+    try {
+        console.log("Updating table delivered status value");
+        await client.connect();
+        await client.queryArray("UPDATE lista SET ideastatus = false, orderstatus = false, deliveredstatus = true WHERE id = $1", id);
+        await client.end();
+        console.log("Delivery status updated");
+    }
+    catch (err) {
+        error.push(err);
+    }
+
 }
 
 const fetchOrders = async (id) => {
-    console.log("itemServices, fetchOrders");
-    await changeOrderService(id);
-    await client.connect();
-    console.log("connected");
-    const res = await client.queryArray('SELECT * from lista WHERE orderstatus = true');
-    await client.end();
-    return res.rows;
+    try {
+        console.log("itemServices, fetchOrders");
+        await changeOrderService(id);
+        await client.connect();
+        console.log("connected");
+        const res = await client.queryArray('SELECT * from lista WHERE orderstatus = true');
+        await client.end();
+        return res.rows;
+    }
+    catch (err) {
+        error.push(err);
+    }
+
 }
 
 const fetchDelivered = async (id) => {
-    console.log("Toimitettujen haku");
-    await changeDeliveredService(id);
-    await client.connect();
-    const ret = await client.queryArray('SELECT * from lista WHERE deliveredstatus = true');
-    console.log("itemService returned");
-    for (const r of ret.rows){
-        console.log(r);
+    try {
+        console.log("Toimitettujen haku");
+        await changeDeliveredService(id);
+        await client.connect();
+        const ret = await client.queryArray('SELECT * from lista WHERE deliveredstatus = true');
+        console.log("itemService returned");
+        for (const r of ret.rows) {
+            console.log(r);
+        }
+        await client.end();
+        return ret.rows;
     }
-    await client.end();
-    
-    return ret.rows;
+    catch (err) {
+        error.push(err);
+    }
+
 }
 
-const deleteAll = async() => {
-    console.log("itemService, deleteAll funkkari");
-    await client.connect();
-    await client.queryArray('DELETE FROM lista');
-    await client.end();
-    
-    const msg = "Kaikki tuhottu";
-    let data = new Response("Kaikki Tuhottu", {
-        status : 200,
-        statusText : "Joulu deletoitu",
-        headers: {
-            "content-type": "text/html",
-          },
-    });
-    
-    return data.statusText;
-    
+const deleteAll = async () => {
+    try {
+        console.log("itemService, deleteAll funkkari");
+        await client.connect();
+        await client.queryArray('DELETE FROM lista');
+        await client.end();
+
+        const msg = "Kaikki tuhottu";
+        let data = new Response("Kaikki Tuhottu", {
+            status: 200,
+            statusText: "Joulu deletoitu",
+            headers: {
+                "content-type": "text/html",
+            },
+        });
+
+        return data.statusText;
+    }
+    catch (err) {
+        error.push(err);
+    }
 }
 
-export {addIdea, fetchIdeas, fetchOrders, fetchDelivered, changeDeliveredService, changeOrderService, deleteAll};
+ensureDir("./errors")
+.then(() => Deno.writeTextFile("./errors/appi_errors_" + Date.now() + ".log", error));
+
+ensureDir("./logs")
+.then(() => Deno.writeTextFile("./logs/log_" + Date.now() + ".log", logs));
+
+
+export { addIdea, fetchIdeas, fetchOrders, fetchDelivered, changeDeliveredService, changeOrderService, deleteAll };
