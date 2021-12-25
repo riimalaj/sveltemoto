eed84f33eb3988e7c5b45d9b7ff1cf9520174584
Jouni <jriimala@gmail.com>
2021-12-25 16:54:50 +0200

    deno.writeTextFile (wip), file created but issue(s) with content

--- routes/controllers/itemController.js
+++ routes/controllers/itemController.js
@@ -1,73 +1,97 @@
-import {Context} from 'https://deno.land/x/oak@v9.0.1/mod.ts';
+import { Context } from 'https://deno.land/x/oak@v9.0.1/mod.ts';
 import * as itemServices from "../../services/itemService.js";
 import { renderFile } from "../../deps.js";
 import { ensureDir, ensureFile, ensureFileSync } from "https://deno.land/std/fs/mod.ts";
+import { format } from "https://deno.land/std@0.91.0/datetime/mod.ts";
 
 
 var log = [];
 
 const showMain = async ({ response }) => {
-    response.body = await renderFile('../views/start.eta');    
+    response.body = await renderFile('../views/start.eta');
 };
 
 
-const addIdea = async({request, response}) => {
-    try{
-    console.log("Idean lisäys");
-    const body = request.body();    
-    const formData = await body.value;
-    const idea = formData.get("toive").trim();
-    const esittaja = formData.get("esittaja").trim();
-    var ideaStatus = Boolean(true);
-    var orderStatus = Boolean(false);
-    var deliveredStatus = Boolean(false);
-    console.log('itemController -> ' + idea + ", " + esittaja + ",ideaStatus-> " + ideaStatus, " ,orderStatus->" + orderStatus + ", deliveredStatus->" + deliveredStatus);
-    await itemServices.addIdea(idea, esittaja, ideaStatus, orderStatus, deliveredStatus);
-    response.redirect('/ideas');
-    const note = new Date() + "_adding idea succeeded";
-    log.push(note);
+const addIdea = async ({ request, response }) => {
+    try {
+        console.log("Idean lisäys");
+        const body = request.body();
+        const formData = await body.value;
+        const idea = formData.get("toive").trim();
+        const esittaja = formData.get("esittaja").trim();
+        var ideaStatus = Boolean(true);
+        var orderStatus = Boolean(false);
+        var deliveredStatus = Boolean(false);
+        console.log('itemController -> ' + idea + ", " + esittaja + ",ideaStatus-> " + ideaStatus, " ,orderStatus->" + orderStatus + ", deliveredStatus->" + deliveredStatus);
+        await itemServices.addIdea(idea, esittaja, ideaStatus, orderStatus, deliveredStatus);
+        response.redirect('/ideas');
+        const note = new Date() + "_adding idea succeeded";
+        log.push(note);
+        loggaus(note);
 
     }
-    catch(err){
+    catch (err) {
         const note = new Date() + "_error: " + err;
         log.push(note);
+        loggaus();
     }
 };
 
-const getIdeas = async({response}) => {
+const getIdeas = async ({ response }) => {
     response.body = await renderFile("../views/ideas.eta", {
         ideas: await itemServices.fetchIdeas(),
     });
 };
 
-const getOrders = async({params, response}) => {        
+const getOrders = async ({ params, response }) => {
     console.log("itemController, getOrders -> params.id = " + params.id);
     response.body = await renderFile("../views/orders.eta", {
         ordered: await itemServices.fetchOrders(params.id),
     });
 };
 
-const getDelivered = async({params, response}) => {    
-        console.log("itemController, getDelivered -> params.id = " + params.id);
-        response.body = await renderFile("../views/delivered.eta",{
+const getDelivered = async ({ params, response }) => {
+    console.log("itemController, getDelivered -> params.id = " + params.id);
+    response.body = await renderFile("../views/delivered.eta", {
         deliveries: await itemServices.fetchDelivered(params.id),
-        });            
+    });
 };
 
-const doDelete = async ({response}) => {
+const doDelete = async ({ response }) => {
     console.log("itemController, doDelete");
     response.body = await renderFile('../views/index.eta', {
-        deleteResp : await itemServices.deleteAll(),
+        deleteResp: await itemServices.deleteAll(),
     });
-    
-    
+
+
 }
 
-export {showMain, getIdeas, getOrders, getDelivered, addIdea, doDelete};
+var temp = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+var tDate = temp.replace(" ", "_").replace(":", "");
+console.log("Dataa tiedostoon logs/appi_logs_" + tDate + ".log");
 
-const today = Date.now();
+const loggaus = (note) => {
+    console.log("loggaus funktioata kutsuttu")
+    ensureDir("./logs")
+        .then(() => {
 
-ensureDir("./logs")
-.then(async () => await Deno.writeTextFile("./logs/appi_logs_" + Date() + ".log", log));
+            let location = "./logs/appi_logs_" + tDate + ".log";
+            console.log("notena -> " + note);
+            Deno.writeTextFile(location, note + "\n");
+            
+            /*
+            for (let i of log) {
+                console.log(i);
+                Deno.writeTextFile(location, i + "\n");
+            }
+            */
+        });
+}
+
+
+
+
+export { showMain, getIdeas, getOrders, getDelivered, addIdea, doDelete };
+
 
 
