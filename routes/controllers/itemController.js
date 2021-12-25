a4462b17f10377099e31abc2ce4881a8f9c337e4
Jouni <jriimala@gmail.com>
2021-12-25 13:33:49 +0200

    no try-catch

--- routes/controllers/itemController.js
+++ routes/controllers/itemController.js
@@ -1,77 +1,61 @@
-import { Context } from 'https://deno.land/x/oak@v9.0.1/mod.ts';
-import * as itemServices from "../../services/itemService.js";
+import {Context} from 'https://deno.land/x/oak@v9.0.1/mod.ts';
+import * as itemServices from "../../services/itemServiceTryCatch.js";
 import { renderFile } from "../../deps.js";
-import { ensureDir } from "https://deno.land/std@0.54.0/fs/ensure_dir.ts";
 
+//No try catch
 
-var error = [];
 
 const showMain = async ({ response }) => {
-    response.body = await renderFile('../views/start.eta');
+    response.body = await renderFile('../views/start.eta');    
 };
 
-const addIdea = async ({ request, response }) => {
-    try {
-        console.log("Idean lisäys");
-        const body = request.body();
-        const formData = await body.value;
-        const idea = formData.get("toive").trim();
-        const esittaja = formData.get("esittaja").trim();
-        var ideaStatus = Boolean(true);
-        var orderStatus = Boolean(false);
-        var deliveredStatus = Boolean(false);
-        console.log('itemController -> ' + idea + ", " + esittaja + ",ideaStatus-> " + ideaStatus, " ,orderStatus->" + orderStatus + ", deliveredStatus->" + deliveredStatus);
-        await itemServices.addIdea(idea, esittaja, ideaStatus, orderStatus, deliveredStatus);
-        response.redirect('/ideas');
-    }
-    catch (err) {
-        error.push(err);
-    }
+
+const addIdea = async({request, response}) => {
+    console.log("Idean lisäys");
+    const body = request.body();    
+    const formData = await body.value;
+    const idea = formData.get("toive").trim();
+    const esittaja = formData.get("esittaja").trim();
+    var ideaStatus = Boolean(true);
+    var orderStatus = Boolean(false);
+    var deliveredStatus = Boolean(false);
+    console.log('itemController -> ' + idea + ", " + esittaja + ",ideaStatus-> " + ideaStatus, " ,orderStatus->" + orderStatus + ", deliveredStatus->" + deliveredStatus);
+    await itemServices.addIdea(idea, esittaja, ideaStatus, orderStatus, deliveredStatus);
+    response.redirect('/ideas');
 };
 
-const getIdeas = async ({ response }) => {
+const getIdeas = async({response}) => {
     response.body = await renderFile("../views/ideas.eta", {
         ideas: await itemServices.fetchIdeas(),
     });
 };
 
-const getOrders = async ({ params, response }) => {
-    try {
-        console.log("itemController, getOrders -> params.id = " + params.id);
-        response.body = await renderFile("../views/orders.eta", {
-            ordered: await itemServices.fetchOrders(params.id),
-        });
-    }
-    catch (err) {
-        error.push(err);
-    }
+const getOrders = async({params, response}) => {        
+    console.log("itemController, getOrders -> params.id = " + params.id);
+    response.body = await renderFile("../views/orders.eta", {
+        ordered: await itemServices.fetchOrders(params.id),
+    });
 };
 
-const getDelivered = async ({ params, response }) => {
-    try {
+const getDelivered = async({params, response}) => {    
         console.log("itemController, getDelivered -> params.id = " + params.id);
-        response.body = await renderFile("../views/delivered.eta", {
-            deliveries: await itemServices.fetchDelivered(params.id),
-        });
-    }
-    catch (err) {
-        error.push(err);
-    }
-
+        response.body = await renderFile("../views/delivered.eta",{
+        deliveries: await itemServices.fetchDelivered(params.id),
+        });            
 };
 
-const doDelete = async ({ response }) => {
-    try {
-        console.log("itemController, doDelete");
-        response.body = await renderFile('../views/index.eta', {
-            deleteResp: await itemServices.deleteAll(),
-        });
-    }
-    catch (err) {
-        error.push(err);
-    }
+const doDelete = async ({response}) => {
+    console.log("itemController, doDelete");
+    response.body = await renderFile('../views/index.eta', {
+        deleteResp : await itemServices.deleteAll(),
+    });
+    
+    
 }
 
+<<<<<<< HEAD
+export {showMain, getIdeas, getOrders, getDelivered, addIdea, doDelete};
+=======
 const today = Date.now();
 
 ensureDir("./errors")
@@ -79,3 +63,4 @@ ensureDir("./errors")
 
 
 export { showMain, getIdeas, getOrders, getDelivered, addIdea, doDelete };
+>>>>>>> 8128d93081c92d70d7ed2bc93faf031300282835
