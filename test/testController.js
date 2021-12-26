8e692063f47376c6629f41b917898e3fd24afdb4
Jouni <jriimala@gmail.com>
2021-12-26 11:38:23 +0200

    deno tests

---
+++ test/testController.js
@@ -0,0 +1,49 @@
+import *  as contra from '../routes/controllers/itemController.js';
+import {existsSync} from "https://deno.land/std/fs/mod.ts";
+import { superoak } from '../deps.js';
+import { app } from '../app.js';
+import { assertEquals } from "https://deno.land/std@0.119.0/testing/asserts.ts";
+
+/*
+Deno.test({
+    name: 'Testing idea',
+    async fn:() => {
+        const request = await superoak(app);
+        await request.get('/ideas').expect(200);
+    },
+    sanitizeOps: false,
+    sanitizeResource: false,
+});
+*/
+
+
+
+
+
+Deno.test({
+    name: 'Testing does log file exist',
+    fn:() => {             
+        const pathFound = existsSync("../logs/appi_logs.log")
+        assertEquals(pathFound, true)
+    },
+    sanitizeOps: false,
+    sanitizeResource: false,
+
+})
+
+Deno.test({
+    name: '\n\nTesting log file content',
+    fn:() => {
+        const file = Deno.open('../logs/appi_logs.log');
+        //const decoder = new TextDecoder('utf-8');
+        const text = Deno.readAll(file);
+
+        if (typeof text == "string" && text.indexOf("added") == true) {
+            var vast = true;
+            console.log("vast variable = " + vast);
+            assertEquals(vast, true)
+        }
+    },
+    sanitizeOps: false,
+    sanitizeResource: false,
+});
